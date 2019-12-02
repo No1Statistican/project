4f894d883e7cbb6c1d195195a99ec18b3d65e395
No1Statistican <30734236+No1Statistican@users.noreply.github.com>
2019-12-01 23:53:29 -0500

    Add files via upload

---
+++ WordChart.R
@@ -0,0 +1,40 @@
+library(wordcloud)
+library(tidyverse)
+library(tidytext)
+library(shinythemes)
+library(shiny)
+
+#load("tidy_all_tweets_cut.Rda")
+
+ui  <- fluidPage(
+  theme = shinytheme('yeti'),
+  titlePanel('Democractic Candidate Twitter Mentions Wordcloud'),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput('cloudCount', '# of Words', min = 50, max = 400, value = 100),
+      hr(),
+      selectInput('CANDIDATE', 'Choose a candidate:', choices = c('PB', 'KH', 'EW', 'BS', 'JB')),
+      hr(),
+      selectInput('STATE', 'Choose a state:', choices = c('NV', 'NH', 'SC', 'IA'))
+    ),
+    mainPanel(
+      plotOutput('wordcloud')
+    )
+  )
+)
+
+server  <- function(input, output) {
+  ngrams  <- reactive({
+    input$ngramCount
+  })
+  
+  output$wordcloud  <- renderPlot({
+    tidy_all_tweets_cut %>%
+      select(word, CANDIDATE, STATE) %>%
+      filter(STATE == input$STATE, CANDIDATE == input$CANDIDATE) %>%
+      count(word) %>%
+      with(wordcloud(word, n, max.words=input$cloudCount, rot.per=.2, colors=c("#cde0c9", "#68b2A0", "#2c6975")))
+  })
+}
+
+shinyApp(ui = ui, server = server)
